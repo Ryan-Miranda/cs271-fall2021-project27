79bf935209b85d91074d2196839acc6f547467eb
Ryan Miranda <ryanm7082@gmail.com>
2021-11-17 13:56:51 -0800

    added A* search, doesnt work for large grid yet?

--- A_star.py
+++ A_star.py
@@ -25,6 +25,9 @@ def return_path(current, grid):
         current = current.parent
     
     path = path[::-1]
+    return path
+
+    # builds a grid based on the path? uneeded for now
     start_value = 0
 
     for i in range(len(path)):
@@ -34,9 +37,8 @@ def return_path(current, grid):
     return result
 
 
-def search(grid, cost, start, end):
+def search(grid, cost, start, walls, boxes, goals):
     start_node = Node(position=tuple(start))
-    end_node = Node(position=tuple(end))
 
     frontier = [start_node]  
     visited = [] 
@@ -44,11 +46,7 @@ def search(grid, cost, start, end):
     outer_iterations = 0
     max_iterations = (len(grid) // 2) ** 10
 
-    move  =  [[-1, 0 ], # up
-              [ 0, -1], # left
-              [ 1, 0 ], # down
-              [ 0, 1 ]] # right
-
+    moves  =  {'up': [-1, 0 ], 'left': [ 0, -1], 'down': [ 1, 0 ], 'right': [ 0, 1 ]} 
     M, N = (len(grid), len(grid[0]))
     
     while len(frontier) > 0:
@@ -68,15 +66,32 @@ def search(grid, cost, start, end):
         frontier.pop(current_index)
         visited.append(current_node)
 
-        if current_node == end_node:
+        if goalTest(boxes, goals):
             return return_path(current_node,grid)
 
         children = []
 
-        for new_position in move: 
+        for move in moves: 
+            new_position = moves[move]
             node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
-            if inGrid(grid, node_position[0], node_position[1]) and not isWall(grid, node_position[0], node_position[1]):
+            if inGrid(grid, node_position) and not isWall(walls, node_position):
+
+                if isBox(boxes, node_position):
+                    
+                    # the new position of the box is just new_position x 2 (in the same direction)
+                    boxNewPosition = (node_position[0] + new_position[0], node_position[1] + new_position[1])
+
+                    # if the box's new position is valid
+                    if inGrid(grid, boxNewPosition) and not isWall(walls, boxNewPosition):
+
+                        # if we push a box, the new position will be the same as current_position, but there will be a new g, f, h assoc with this state
+                        updateBoxLocation(boxes, node_position, boxNewPosition)
+                        node_position = current_node.position
+
+                    else:
+                        continue
+                
                 new_node = Node(current_node, node_position)
                 children.append(new_node)
 
@@ -85,7 +100,7 @@ def search(grid, cost, start, end):
             if child not in visited:
 
                 child.g = current_node.g + cost
-                child.h = euclid(child.position[0], child.position[1], end_node.position[0], end_node.position[1])
+                child.h = manhattanDistance(goals, boxes)
                 child.f = child.g + child.h
 
                 if len([i for i in frontier if child == i and child.g > i.g]) > 0:
@@ -93,21 +108,53 @@ def search(grid, cost, start, end):
 
                 frontier.append(child)
 
+    if goalTest(boxes, goals):
+        return return_path(current_node,grid)
+    else:
+        print('ERROR: no goal found but exiting anyway??')
+
+
+def goalTest(boxes, goals):
+    # if the set of goal coords = set of box coords, every box is in some goal and the game is finished
+    return set(boxes) == set(goals)
+
+
+def updateBoxLocation(boxes, oldBoxPosition, newBoxPosition):
+    boxes.remove(oldBoxPosition)
+    boxes.add(newBoxPosition)
+
+
+def manhattanDistance(goals, boxes):
+    boxes_copy = list(boxes)
+    dist = 0
+
+    for goal in goals: 
+        box, d = min( [(b, calcManhattan(b, goal)) for b in boxes_copy], key=lambda t: t[1] )
+        dist += d
+        boxes_copy.remove(box)
+    
+    return dist
+
+def calcManhattan(box, goal):
+    return abs(box[0] - goal[0]) + abs(box[1] - goal[1])
+
+
+def inGrid(grid, position):
+    return position[0] > -1 and position[0] < len(grid) and position[1] > -1 and position[1] < len(grid[0])
+
+def isWall(walls, position):
+    return position in walls
+
+def isBox(boxes, position):
+    return position in boxes
+
+def isStorage(goals, position):
+    return position in goals
+
 
 def euclid(x1, y1, x2, y2):
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
-def inGrid(grid, i, j):
-    return i > -1 and i < len(grid) and j > -1 and j < len(grid[0])
-
-def isWall(grid, i, j):
-    return grid[i][j] == '#'
-
-def isBox(grid, i, j):
-    return grid[i][j] == '$'
-    
-def isStorage(grid, i, j):
-    return grid[i][j] == '.'
 
 def printPath(path):
     for i in range(len(path)):
@@ -116,16 +163,17 @@ def printPath(path):
         print()
 
 
-if __name__ == '__main__':
-    grid = [[0, '#', 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, '#', 0, '#', 0, 0],
-            [0, '#', 0, 0, '#', 0],
-            [0, 0, 0, 0, '#', 0]]
-    
-    start = [0, 0] # starting position
-    end = [4,5] # ending position
+def doSearch(grid, walls, boxes, goals, X0, Y0):
+    start = [X0, Y0]
     cost = 1 # cost per movement
 
-    path = search(grid,cost, start, end)
-    printPath(path)
+    boxes = set(boxes)
+    goals = set(goals)
+    walls = set(walls)
+
+    path = search(grid, cost, start, walls, boxes, goals)
+
+    print(f'Final location of boxes: {boxes}')
+    print(f'Goal locations: {goals}')
+    print(f'Path: {path}')
+    print()
--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+import A_star
+
 def readInput():
     M, N = [int(num) for num in input().split(' ')]
 
@@ -5,7 +7,7 @@ def readInput():
     boxes = getCoordinates( input().split(' ') )
     storageLocations = getCoordinates( input().split(' ') )
 
-    X0, Y0 = [int(num) for num in input().split(' ')]
+    X0, Y0 = [int(num)-1 for num in input().split(' ')]
 
     return M, N, wallSquares, boxes, storageLocations, X0, Y0
 
@@ -14,7 +16,7 @@ def getCoordinates(inputList):
     inputList = [int(coord) for coord in inputList]
     coords = []
     for i in range(1, inputList[0] * 2, 2 ):
-        coords.append( (inputList[i], inputList[i+1]) )
+        coords.append( (inputList[i] - 1, inputList[i+1] - 1) )
     return coords
 
 
@@ -24,12 +26,12 @@ def buildGrid(M, N, wallSquares=0, boxes=0, storageLocations=0, X0=0, Y0=0):
     # (1, 1) at top left of grid, we do a simple conversion by subtracting 1 from every index
     
     for i, j in wallSquares:
-        grid[i-1][j-1] = '#'
+        grid[i][j] = '#'
     for i, j in boxes:
-        grid[i-1][j-1] = '$'
+        grid[i][j] = '$'
     for i, j in storageLocations:
-        grid[i-1][j-1] = '.'
-    grid[X0-1][Y0-1] = '@'
+        grid[i][j] = '.'
+    grid[X0][Y0] = '@'
 
     return grid
 
@@ -47,3 +49,5 @@ if __name__ == '__main__':
     M, N, wallSquares, boxes, storageLocations, X0, Y0 = readInput()
     grid = buildGrid(M, N, wallSquares, boxes, storageLocations, X0, Y0)
     printGrid(grid)
+
+    A_star.doSearch(grid, wallSquares, boxes, storageLocations, X0, Y0)
